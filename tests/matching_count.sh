a811a97e73a28132d59196d6f9eb0c157a8d3a71
Waldo Jaquith <waldo@jaquith.org>
2017-04-15 00:11:19 -0400

    Tweak Bash syntax

--- tests/matching_count.sh
+++ tests/matching_count.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILE_LENGTH="$(wc -l zipcodes.csv |cut -d " " -f 4)"
-FILE_LENGTH=$(($FILE_LENGTH - 1))
+FILE_LENGTH="$((FILE_LENGTH - 1))"
 DIR_COUNT="$(ls api/ |wc -l)"
 
 if [ "$FILE_LENGTH" -ne "$DIR_COUNT" ]; then
