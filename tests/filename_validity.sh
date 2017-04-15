216b1dee0c20bc21a3469c302c00811a87c78dd5
Waldo Jaquith <waldo@jaquith.org>
2017-04-15 00:05:06 -0400

    Make tests' output more verbose

--- tests/filename_validity.sh
+++ tests/filename_validity.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OUTPUT="$(ls api/ |egrep -v [0-9]{5}\.json)"
-if [ ${#OUTPUT} -ge 1 ]
-	then exit 1
+if [ ${#OUTPUT} -ge 1 ]; then
+	echo "The following filenames are invalid: $OUTPUT"
+	exit 1
 fi
--- tests/matching_count.sh
+++ tests/matching_count.sh
@@ -4,6 +4,7 @@ FILE_LENGTH="$(wc -l zipcodes.csv |cut -d " " -f 4)"
 FILE_LENGTH=$(($FILE_LENGTH - 1))
 DIR_COUNT="$(ls api/ |wc -l)"
 
-if [ "$FILE_LENGTH" -ne "$DIR_COUNT" ]
-	then exit 1
+if [ "$FILE_LENGTH" -ne "$DIR_COUNT" ]; then
+	echo "There are $FILE_LENGTH CSV entries, but $DIR_COUNT JSON files."
+	exit 1
 fi
