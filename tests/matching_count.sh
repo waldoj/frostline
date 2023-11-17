a775d2356c945b914635b7de08b32ab657ede7e7
Waldo Jaquith <waldo@jaquith.org>
2023-11-16 22:12:28 -0500

    Fix filename in test

--- tests/matching_count.sh
+++ tests/matching_count.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-FILE_LENGTH="$(wc -l zipcodes.csv |sed -e 's/^[ \t]*//' |cut -d " " -f 1)"
+FILE_LENGTH="$(wc -l combined_zipcodes.csv |sed -e 's/^[ \t]*//' |cut -d " " -f 1)"
 FILE_LENGTH="$((FILE_LENGTH - 1))"
+#### NEXT UP: SUBTRACT number of zipcodes with no PHZ data: 2536
 DIR_COUNT="$(ls api/ |wc -l)"
 
 if [ "$FILE_LENGTH" -ne "$DIR_COUNT" ]; then
